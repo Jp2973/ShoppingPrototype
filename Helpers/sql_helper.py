008a453b22c3061312f921ae5daba96bd5a56ae2
Caden Austin <cadenbryant@hotmail.com>
2022-05-01 22:37:47 -0400

    csv parser

--- Helpers/sql_helper.py
+++ Helpers/sql_helper.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import sessionmaker
 import pandas as pd
 
 DB_FILE_NAME = "/database.db"
-PARENT_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CURRENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIRECTORY = os.path.dirname(CURRENT_DIRECTORY)
 
 sys.path.append(PARENT_DIRECTORY)
 from Models import *
@@ -14,20 +15,42 @@ engine = create_engine('sqlite://' + DB_FILE_NAME)
 
 _SESSION = sessionmaker(bind=engine)
 
+def createBook(bookInfo):
+    session = getSession()
+    bookInstance = Book(author=bookInfo["author"], publisher=bookInfo["publisher"], isbn=bookInfo["isbn"])    
+    session.add(bookInstance)
+    session.commit()
+
+    book = session.query(Book).filter_by(isbn=bookInfo["isbn"]).first()
+    itemInstance = InventoryItem(quantity=bookInfo["quantity"], title=bookInfo["title"], description=bookInfo["description"], genre=bookInfo["genre"], price=bookInfo["price"], item_type="B", book_reference=book.id)
+    session.add(itemInstance)
+    session.commit()
+
+def createMovie(movieInfo):
+    session = getSession()
+    movieInstance = Movie(director=movieInfo["director"], leading_actor=movieInfo["leading_actor"])    
+    session.add(movieInstance)
+    session.commit()
+
+    session.refresh(movieInstance)
+
+    itemInstance = InventoryItem(quantity=movieInfo["quantity"], title=movieInfo["title"], description=movieInfo["description"], genre=movieInfo["genre"], price=movieInfo["price"], item_type="M", movie_reference=movieInstance.id)
+    session.add(itemInstance)
+    session.commit()
+
 def createDatabase():
     dropTables()
     session = getSession()
     insp = inspect(engine)
     print(insp.get_table_names())
 
-    bookInstance = Book(author="John Doe", publisher="Wholehouse", isbn="10928901")    
-    session.add(bookInstance)
-    session.commit()
+    books_df = pd.read_csv(CURRENT_DIRECTORY + "/Initial/Books.csv")
+    books_df_dict = books_df.to_dict(orient='records')
+    [createBook(book) for book in books_df_dict]
 
-    book = session.query(Book).filter_by(author="John Doe").first()
-    itemInstance = InventoryItem(quantity=7, title="Generic Title", description="Generic Description", genre="Generic Genre", price=20.70, item_type="B", book_reference=book.id)
-    session.add(itemInstance)
-    session.commit()
+    movies_df = pd.read_csv(CURRENT_DIRECTORY + "/Initial/Movies.csv")
+    movies_df_dict = movies_df.to_dict(orient='records')
+    [createMovie(movie) for movie in movies_df_dict]
 
     print(session.query(Book).all())
 
@@ -40,11 +63,13 @@ def dropTables():
 
 def printTables():
     books = pd.read_sql_table(table_name="Book", con=engine)
+    movies = pd.read_sql_table(table_name="Movie", con=engine)
     items = pd.read_sql_table(table_name="InventoryItem", con=engine)
     carts = pd.read_sql_table(table_name="ShoppingCart", con=engine)
     customers = pd.read_sql_table(table_name="Customer", con=engine)
 
     print(books)
+    print(movies)
     print(items)
     print(carts)
     print(customers)
